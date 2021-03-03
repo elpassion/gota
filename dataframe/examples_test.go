2922ca696f19a67d06b09ebf37aa178309e24c4e
Karol Sarnacki <616132+karolsarnacki@users.noreply.github.com>
2021-03-03 14:19:12 +0100

    Update examples_test.go

--- dataframe/examples_test.go
+++ dataframe/examples_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
+	"github.com/elpassion/gota/dataframe"
+	"github.com/elpassion/gota/series"
 )
 
 func ExampleNew() {
